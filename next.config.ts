d3283ec1fa401989c4c15752a41f3fbb0ada7927
Farriel Arrianta <farrielarrianta@gmail.com>
2026-09-05 19:56:47 +0700

    feat: add URL rewrites for v2 path to external portfolio site

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+  return [
+    {
+      source: "/v2/:path*",
+      destination: "https://my-psvita-portfolio.vercel.app/v2/:path*",
+    },
+  ];
+},
 };
 
 export default nextConfig;
